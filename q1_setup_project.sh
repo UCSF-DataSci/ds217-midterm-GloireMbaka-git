a63f8a3654c9e13d0176047789761721a0a5ff6d
GloireMbaka-git <gloire.mbakaonya@ucsf.edu>
2025-10-25 17:42:29 +0000

    Completed Mid-term

--- q1_setup_project.sh
+++ q1_setup_project.sh
@@ -2,16 +2,31 @@
 # Assignment 5, Question 1: Project Setup Script
 # This script creates the directory structure for the clinical trial analysis project
 # TODO: Make this script executable (if not already)
-# chmod +x q1_setup_project.sh
+chmod +x q1_setup_project.sh
 
 # TODO: Create the following directories:
+#!/bin/bash
 #   - data/
+mkdir -p data
+
 #   - output/
+mkdir -p output
+
 #   - reports/
+mkdir -p reports
+
 
 # TODO: Generate the dataset
 #       Run: python3 generate_data.py
+python3 generate_data.py
 #       This creates data/clinical_trial_raw.csv with 10,000 patients
 
 # TODO: Save the directory structure to reports/directory_structure.txt
 #       Hint: Use 'ls -la' or 'tree' command
+
+ls -la
+
+# Save directory structure with tree
+tree -a > reports/directory_structure.txt
+
+
